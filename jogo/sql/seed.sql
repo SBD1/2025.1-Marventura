13868a51ad81e331334f0b08fdc839d7f0bb41cd
F1reFinger <helderbonzinho@gmail.com>
2025-06-12 22:20:03 -0300

    alteração nos requisitos e tbm criado um populador

---
+++ jogo/sql/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO ilha (nome, descricao) VALUES ('Ilha das Sombras', 'Uma ilha misteriosa...');
+INSERT INTO mar (nome) VALUES ('Mar Abissal');
+INSERT INTO porto (nome, ilha_id) VALUES ('Porto Negro', 1);
