6ea8535dd1ad3867ae9c89743d162f4a70dd199d
MatheusHenrickSantos <79066829+MatheusHenrickSantos@users.noreply.github.com>
2025-06-16 22:37:46 -0300

    Revisão de Pull Request

--- jogo/banco-de-dados/ddl.sql
+++ jogo/banco-de-dados/ddl.sql
@@ -1,56 +1,60 @@
-CREATE TABLE Efeito (
-    IdentificadorEfeito INT PRIMARY KEY,
-    Nome VARCHAR(255),
-    Valor VARCHAR(255)
+CREATE TABLE efeito (
+    identificador_efeito SMALLINT SERIAL PRIMARY KEY,
+    nome CHAR(100) NOT NULL,
+    valor SMALLINT NOT NULL
 );
 
-CREATE TABLE Consumivel (
-    IdentificadorConsumivel INT PRIMARY KEY,
-    Nome VARCHAR(255),
-    Tipo VARCHAR(255),
-    Quantidade INT,
-    Raridade VARCHAR(50),
-    PrecoCompra INT,
-    PrecoVenda INT,
-    EFabricavel BOOLEAN
+CREATE TABLE consumivel (
+    identificador_consumivel SMALLINT PRIMARY KEY,
+    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('con')),
+    nome CHAR(100) NOT NULL,
+    descricao CHAR(100) NOT NULL,
+    quantidade SMALLINT NOT NULL,
+    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (Raridade IN ('★', '★★', '★★★')),
+    local_encontrado CHAR(100) NOT NULL CHECK (local_encontrado IN ('Campos', 'Cidade', 'Neve', 'Deserto', 'Ilha Assombrada', 'Fortaleza da Marinha')),
+    preco_de_compra SMALLINT NOT NULL,
+    preco_de_venda SMALLINT NOT NULL,
+    e_fabricavel BOOLEAN
 );
 
-CREATE TABLE NaoConsumivel (
-    IdentificadorNaoConsumivel INT PRIMARY KEY,
-    Nome VARCHAR(255),
-    Tipo VARCHAR(255),
-    Quantidade INT,
-    Raridade VARCHAR(50),
-    PrecoCompra INT,
-    PrecoVenda INT
+CREATE TABLE nao_consumivel (
+    identificador_nao_consumivel SMALLINT PRIMARY KEY,
+    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('ncn')),
+    nome CHAR(100) NOT NULL,
+    descricao CHAR(100) NOT NULL,
+    quantidade SMALLINT NOT NULL,
+    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (Raridade IN ('★', '★★', '★★★')),
+    local_encontrado CHAR(100) NOT NULL CHECK (local_encontrado IN ('Campos', 'Cidade', 'Neve', 'Deserto', 'Ilha Assombrada', 'Fortaleza da Marinha')),
+    preco_de_compra SMALLINT NOT NULL,
+    preco_de_venda SMALLINT NOT NULL,
 );
 
-CREATE TABLE EfeitoConsumivel (
-    IdentificadorConsumivel INT,
-    IdentificadorEfeito INT,
-    PRIMARY KEY (IdentificadorConsumivel, IdentificadorEfeito),
-    FOREIGN KEY (IdentificadorConsumivel) REFERENCES Consumivel(IdentificadorConsumivel),
-    FOREIGN KEY (IdentificadorEfeito) REFERENCES Efeito(IdentificadorEfeito)
+CREATE TABLE efeito_consumivel (
+    identificador_consumivel SMALLINT,
+    identificador_efeito SMALLINT,
+    PRIMARY KEY (identificador_consumivel, identificador_efeito),
+    FOREIGN KEY (identificador_consumivel) REFERENCES consumivel(identificador_consumivel),
+    FOREIGN KEY (identificador_efeito) REFERENCES efeito(identificador_efeito)
 );
 
-CREATE TABLE Receita (
-    IdentificadorReceita INT PRIMARY KEY,
-    IdentificadorConsumivelProduzido INT,
-    FOREIGN KEY (IdentificadorConsumivelProduzido) REFERENCES Consumivel(IdentificadorConsumivel)
+CREATE TABLE receita (
+    identificador_receita SMALLINT PRIMARY KEY,
+    consumivel_produzido SMALLINT,
+    FOREIGN KEY (consumivel_roduzido) REFERENCES consumivel(identificador_consumivel)
 );
 
-CREATE TABLE IngredienteConsumivel (
-    IdentificadorReceita INT,
-    IdentificadorConsumivel INT,
-    PRIMARY KEY (IdentificadorReceita, IdentificadorConsumivel),
-    FOREIGN KEY (IdentificadorReceita) REFERENCES Receita(IdentificadorReceita),
-    FOREIGN KEY (IdentificadorConsumivel) REFERENCES Consumivel(IdentificadorConsumivel)
+CREATE TABLE ingrediente_consumivel (
+    identificador_receita SMALLINT,
+    identificador_consumivel SMALLINT,
+    PRIMARY KEY (identificador_receita, identificador_consumivel),
+    FOREIGN KEY (identificador_receita) REFERENCES receita(identificador_receita),
+    FOREIGN KEY (identificador_consumivel) REFERENCES consumivel(identificador_consumivel)
 );
 
-CREATE TABLE IngredienteNaoConsumivel (
-    IdentificadorReceita INT,
-    IdentificadorNaoConsumivel INT,
-    PRIMARY KEY (IdentificadorReceita, IdentificadorNaoConsumivel),
-    FOREIGN KEY (IdentificadorReceita) REFERENCES Receita(IdentificadorReceita),
-    FOREIGN KEY (IdentificadorNaoConsumivel) REFERENCES NaoConsumivel(IdentificadorNaoConsumivel)
+CREATE TABLE ingrediente_nao_consumivel (
+    identificador_receita SMALLINT,
+    identificador_nao_consumivel SMALLINT,
+    PRIMARY KEY (identificador_receita, identificador_nao_consumivel),
+    FOREIGN KEY (identificador_receita) REFERENCES receita(identificador_receita),
+    FOREIGN KEY (identificador_nao_consumivel) REFERENCES nao_consumivel(identificador_nao_consumivel)
 );
