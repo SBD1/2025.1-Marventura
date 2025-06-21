d8d7a34912b5bfb8c0ef7c7b6b9cef315cdb21b9
MatheusHenrickSantos <79066829+MatheusHenrickSantos@users.noreply.github.com>
2025-06-21 14:54:38 -0300

    Corrige restrições e move tabela habilidade para cima A tabela habilidade precisava estar antes de tabelas que dependiam dela, como arma e fruta.

--- jogo/banco-de-dados/ddl.sql
+++ jogo/banco-de-dados/ddl.sql
@@ -1,15 +1,15 @@
 CREATE TABLE tipo_item (
     identificador_item ID PRIMARY KEY,
-    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('ace', 'arm', 'fru', 'con', 'ncn'))
+    tipo CHAR(3) NOT NULL CHECK (tipo IN ('ace', 'arm', 'fru', 'con', 'ncn'))
 );
 
 CREATE TABLE acessorio (
     identificador_acessorio ID PRIMARY KEY REFERENCES tipo_item(identificador_item),
-    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('ace')),
+    tipo CHAR(3) NOT NULL CHECK (tipo IN ('ace')),
     nome CHAR(50) NOT NULL,
     descricao CHAR(150) NOT NULL,
     quantidade SMALLINT NOT NULL,
-    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (Raridade IN ('★', '★★', '★★★')),
+    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (raridade IN ('★', '★★', '★★★')),
     local_encontrado CHAR(25) NOT NULL CHECK (local_encontrado IN ('Ilha de Borabóia', 'Cidade de Lurien', 'Ilha Glacial de Frimora', 'Cactuaraquara', 'Nublária', 'Quartel Naval D-57')),
     preco_de_compra SMALLINT NOT NULL,
     preco_de_venda SMALLINT NOT NULL
@@ -20,14 +20,28 @@ BEFORE INSERT ON acessorio
 FOR EACH ROW
 EXECUTE FUNCTION public.gerar_id();
 
+CREATE TABLE habilidade (
+    identificador_habilidade ID PRIMARY KEY,
+    nome CHAR(50) NOT NULL,
+    descricao CHAR(150) NOT NULL,
+    tipo_de_ataque CHAR(10) NOT NULL CHECK (tipo_de_ataque IN ('soco', 'espada', 'estilingue', 'fruta')),
+    dano SMALLINT NOT NULL,
+    custo SMALLINT NOT NULL
+);
+
+CREATE TRIGGER atribui_id_habilidade
+BEFORE INSERT ON habilidade
+FOR EACH ROW
+EXECUTE FUNCTION public.gerar_id();
+
 CREATE TABLE arma (
     identificador_arma ID PRIMARY KEY REFERENCES tipo_item(identificador_item),
     identificador_habilidade ID REFERENCES habilidade(identificador_habilidade),
-    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('arm')),
+    tipo CHAR(3) NOT NULL CHECK (tipo IN ('arm')),
     nome CHAR(50) NOT NULL,
     descricao CHAR(150) NOT NULL,
     quantidade SMALLINT NOT NULL,
-    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (Raridade IN ('★', '★★', '★★★')),
+    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (raridade IN ('★', '★★', '★★★')),
     local_encontrado CHAR(4) NOT NULL CHECK (local_encontrado IN ('Loja')),
     preco_de_compra SMALLINT NOT NULL,
     preco_de_venda SMALLINT NOT NULL
@@ -41,11 +55,11 @@ EXECUTE FUNCTION public.gerar_id();
 CREATE TABLE fruta (
     identificador_fruta ID PRIMARY KEY REFERENCES tipo_item(identificador_item),
     identificador_habilidade ID REFERENCES habilidade(identificador_habilidade),
-    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('fru')),
+    tipo CHAR(3) NOT NULL CHECK (tipo IN ('fru')),
     nome CHAR(50) NOT NULL,
     descricao CHAR(150) NOT NULL,
     quantidade SMALLINT NOT NULL,
-    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (Raridade IN ('★', '★★', '★★★')),
+    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (raridade IN ('★', '★★', '★★★')),
     local_encontrado CHAR(25) NOT NULL CHECK (local_encontrado IN ('Missão', 'Evento')),
     preco_de_compra SMALLINT NOT NULL,
     preco_de_venda SMALLINT NOT NULL
@@ -58,11 +72,11 @@ EXECUTE FUNCTION public.gerar_id();
 
 CREATE TABLE consumivel (
     identificador_consumivel ID PRIMARY KEY REFERENCES tipo_item(identificador_item),
-    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('con')),
+    tipo CHAR(3) NOT NULL CHECK (tipo IN ('con')),
     nome CHAR(50) NOT NULL,
     descricao CHAR(150) NOT NULL,
     quantidade SMALLINT NOT NULL,
-    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (Raridade IN ('★', '★★', '★★★')),
+    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (raridade IN ('★', '★★', '★★★')),
     local_encontrado CHAR(25) NOT NULL CHECK (local_encontrado IN ('Ilha de Borabóia', 'Cidade de Lurien', 'Ilha Glacial de Frimora', 'Cactuaraquara', 'Nublária', 'Quartel Naval D-57')),
     preco_de_compra SMALLINT NOT NULL,
     preco_de_venda SMALLINT NOT NULL,
@@ -76,11 +90,11 @@ EXECUTE FUNCTION public.gerar_id();
 
 CREATE TABLE nao_consumivel (
     identificador_nao_consumivel ID PRIMARY KEY REFERENCES tipo_item(identificador_item),
-    tipo CHAR(3) NOT NULL CHECK (Tipo IN ('ncn')),
+    tipo CHAR(3) NOT NULL CHECK (tipo IN ('ncn')),
     nome CHAR(50) NOT NULL,
     descricao CHAR(150) NOT NULL,
     quantidade SMALLINT NOT NULL,
-    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (Raridade IN ('★', '★★', '★★★')),
+    raridade CHAR(3) DEFAULT '★' NOT NULL CHECK (raridade IN ('★', '★★', '★★★')),
     local_encontrado CHAR(25) NOT NULL CHECK (local_encontrado IN ('Ilha de Borabóia', 'Cidade de Lurien', 'Ilha Glacial de Frimora', 'Cactuaraquara', 'Nublária', 'Quartel Naval D-57')),
     preco_de_compra SMALLINT NOT NULL,
     preco_de_venda SMALLINT NOT NULL
@@ -140,20 +154,7 @@ CREATE TABLE efeito_consumivel (
     identificador_efeito ID,
     identificador_consumivel ID,
     PRIMARY KEY (identificador_efeito, identificador_consumivel),
-    FOREIGN KEY (identificador_efeito) REFERENCES efeito(identificador_efeito)
-    FOREIGN KEY (identificador_consumivel) REFERENCES consumivel(identificador_consumivel),
+    FOREIGN KEY (identificador_efeito) REFERENCES efeito(identificador_efeito),
+    FOREIGN KEY (identificador_consumivel) REFERENCES consumivel(identificador_consumivel)
 );
 
-CREATE TABLE habilidade (
-    identificador_habilidade ID PRIMARY KEY,
-    nome CHAR(50) NOT NULL,
-    descricao CHAR(150) NOT NULL,
-    tipo_de_ataque CHAR(10) NOT NULL CHECK (Tipo IN ('soco', 'espada', 'estilingue', 'fruta')),
-    dano SMALLINT NOT NULL,
-    custo SMALLINT NOT NULL
-);
-
-CREATE TRIGGER atribui_id_habilidade
-BEFORE INSERT ON habilidade
-FOR EACH ROW
-EXECUTE FUNCTION public.gerar_id();
