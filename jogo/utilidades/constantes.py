df5f823c4dbc15c0e810154d3ce244d5fd5f1e67
Matheus Henrick Santos <190018101@aluno.unb.br>
2025-07-01 19:25:26 -0300

    Atualiza constantes e requisitos

--- jogo/utilidades/constantes.py
+++ jogo/utilidades/constantes.py
@@ -177,5 +177,5 @@ CHAVE_CENARIO_ILHA_6_PARTE_2 = 'cenario_quartel_interior'
 CHAVE_CENARIO_ILHA_6_PARTE_3 = 'cenario_quartel_escritorio'
 
 # --- Debugging ---
-DEBUG_DESENHAR_CAIXAS_COLISAO = True # (True para exibir, False para ocultar)
+DEBUG_DESENHAR_CAIXAS_COLISAO = False # (True para exibir, False para ocultar)
 COR_CAIXA_COLISAO = (255, 0, 255) # Magenta para caixas de colisão
