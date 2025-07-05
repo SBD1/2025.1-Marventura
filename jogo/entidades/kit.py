b255904ea2fec3ccc9f5807abe907f37fd14a0a9
Matheus Henrick Santos <190018101@aluno.unb.br>
2025-07-05 15:09:10 -0300

    Cria entidade kit do explorador

---
+++ jogo/entidades/kit.py
@@ -0,0 +1,25 @@
+class KitDoExplorador:
+    def __init__(self, kit):
+        self.arma = None  # dict com dados da arma (já carregado do banco)
+        self.fruta = None
+        self.acessorio = None
+
+        self.equipar(kit)
+       
+
+    def equipar(self, kit):
+        for item in kit:
+            match item.tipo_item:
+                case "ace":
+                    self.acessorio = item
+                case "fru":
+                    self.fruta = item
+                case "arm":
+                    self.arma = item
+
+    def obter_ids_do_equipamento(self):
+        return {
+            "id_arma": self.arma.identificador_item if self.arma else None,
+            "id_fruta": self.fruta.identificador_item if self.fruta else None,
+            "id_acessorio": self.acessorio.identificador_item if self.acessorio else None
+        }
